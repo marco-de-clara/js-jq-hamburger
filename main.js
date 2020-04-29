b5e55449345f22a7309b05394527d0e4d94caecb
Marco De Clara <marcodeclara@gmail.com>
2020-04-29 14:08:58 +0200

    Added hamburger menu script

---
+++ main.js
@@ -0,0 +1,9 @@
+// toggle between adding and removing .active class to fa-bars (hamburger menu icon)
+$(".fa-bars").click(function() {
+	  $(".hamburger-menu").toggleClass("active");
+  });
+
+// toggle between adding and removing .active class to fa-times (X icon)
+$(".fa-times").click(function() {
+	  $(".hamburger-menu").toggleClass("active");
+  });
